9ad5c34e202a447af880d5cc727e42172755d292
Charlie Sanders <sanderscharlie@gmail.com>
2013-07-11 23:48:43 -0500

    Move sorting correctness example to shared example group

--- spec/sort/quicksort_spec.rb
+++ spec/sort/quicksort_spec.rb
@@ -1,11 +1,17 @@
 require 'sort/quicksort'
 
-describe Quicksort do
-  subject { described_class.sort input.clone }
+shared_examples "a correct sorting algorithm" do
+  context "when sorting 1000 random integers" do
+    let(:input) { Array.new(1_000) { rand 1_000_000_000 } }
 
-  let(:input) { Array.new(1_000) { rand 1_000_000_000 } }
-
-  it 'sorts correctly' do
-    should == input.sort
+    it 'sorts correctly' do
+      output.should == input.sort
+    end
   end
+end
+
+describe Quicksort do
+  let(:output) { described_class.sort input }
+
+  it_behaves_like "a correct sorting algorithm"
 end
