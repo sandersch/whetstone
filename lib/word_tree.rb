26fe8446101b6b8a3a839c6c86f910d01ecd8f54
Charlie Sanders <sanderscharlie@gmail.com>
2013-08-08 21:17:17 -0500

    Change implementation from linear search to trie with nested hash. Gives false positives for partial word matches

--- lib/word_tree.rb
+++ lib/word_tree.rb
@@ -1,13 +1,26 @@
 class WordTree
   def initialize
-    @dict = []
+    @tree = {}
   end
 
+  attr_reader :tree
+
   def store(dict)
-    @dict = dict
+    @tree = dict.group_by { |w| w[0] }.each_with_object({}) do |(first_letter,words), hash| 
+      hash[first_letter] = store(remove_first_character_from words)
+    end
   end
 
   def contains?(word)
-    @dict.include? word
+    !!word.chars.each_with_object(current_dict=tree) do |char|
+      current_dict = current_dict[char]
+      return false unless current_dict
+    end
+  end
+
+  private
+
+  def remove_first_character_from(words)
+    words.select { |w| w.size > 1 }.map { |w| w[1..-1] }
   end
 end
