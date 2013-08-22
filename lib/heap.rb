8a1a0cb574421dffd3aa3d3dbcf301e12bee262e
Charlie Sanders <sanderscharlie@gmail.com>
2013-08-21 23:44:02 -0500

    Fix child/parent methods to work on zero based indexes and no one based indexes

--- lib/heap.rb
+++ lib/heap.rb
@@ -17,11 +17,21 @@ class Heap
     nodes.shift
   end
 
+  def satisfies_heap_property_at?(idx)
+    puts "nodes = #{self.nodes.inspect}"
+    self.class.children_of(idx).each do |child|
+      @nodes[child] && (@nodes[idx] < @nodes[child] or return false)
+    end
+    true
+  end
+
   def self.parent_of(idx)
-    (idx/2.0).floor
+    return nil if idx == 0
+
+    (idx-1) / 2
   end
 
   def self.children_of(idx)
-    [2 * idx, 2 * idx + 1 ]
+    [2 * idx + 1, 2 * idx + 2]
   end
 end
--- spec/heap_spec.rb
+++ spec/heap_spec.rb
@@ -14,10 +14,17 @@ describe Heap, 'a minimum heap' do
       heap.insert(element)
     end
 
-    let(:element) { mock :element }
+    let(:element) { 1 }
 
     its(:size) { should == 1 }
     its(:extract_min) { should be element }
+
+    describe "testing that the heap property holds" do
+      subject { heap.satisfies_heap_property_at? index }
+      let(:index) { 0 }
+
+      it { should be_true }
+    end
   end
 
   pending "with many elements" do
@@ -30,32 +37,84 @@ describe Heap, 'a minimum heap' do
     let(:elements) { [5, 2, 3, 1, 4] }
 
     its(:size) { should == 5 }
-    its(:extract_min) { should == 1 }
+    its(:extract_min) { pending; should == 1 }
+  end
+
+  describe "testing that the heap property holds" do
+    subject { heap.satisfies_heap_property_at? index }
+
+    context "at a leaf node" do
+      let(:index) { 0 }
+
+      it { should be_true }
+    end
+
+    context "at a node with one child" do
+      let(:index) { 1 }
+
+      context "that is greater than the node" do
+        before do
+          heap.instance_eval { @nodes = [0, 1, 2, 3] }
+        end
+
+        it { should be_true }
+      end
+
+      context "that is less than the node" do
+        before do
+          heap.instance_eval { @nodes = [0, 3, 2, 1] }
+        end
+
+        it { should be_false }
+      end
+    end
+
+    context "at a node with two children" do
+      context "when a child is less than the node"
+
+      context "when both children are greater than the node"
+    end
   end
 end
 
 describe Heap, "class methods" do
-  let(:k) { rand(1000) }
-
   describe "determining the parent of" do
-    let(:subject) { described_class.parent_of(index) }
+    subject { described_class.parent_of(index) }
+    let(:k) { rand(1..100) }
 
-    context "an even index" do
-      let(:index) { 2 * k }
+    context "index 0" do
+      let(:index) { 0 }
+
+      it { should be_nil }
+    end
+
+    context "an odd index" do
+      let(:index) { 2*k + 1 }
 
       it { should == k }
     end
 
-    context "an odd index" do
-      let(:index) { 2 * k + 1}
+    context "an even index" do
+      let(:index) { 2*k + 2 }
 
       it { should == k }
     end
   end
 
   describe "determining the children of an element" do
-    let(:subject) { described_class.children_of(k) }
+    let(:subject) { described_class.children_of(index) }
 
-    it { should == [2 * k, 2 * k + 1] }
+    context "at index 0" do
+      let(:index) { 0 }
+
+      it { should == [1, 2] }
+    end
+
+    context "at a random index" do
+      let(:k) { rand(0..100) }
+      let(:index) { k }
+
+      it { should == [2 * k + 1, 2 * k + 2] }
+    end
   end
 end
